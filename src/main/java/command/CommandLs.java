90d37ea27044c6e934a536dfd8d9837de628f62d
gabisaldivar <saldivargaby18@gmail.com>
2019-05-23 22:18:51 +0300

    Finished Ls

--- src/main/java/command/CommandLs.java
+++ src/main/java/command/CommandLs.java
@@ -1,5 +1,9 @@
 package command;
+import org.kohsuke.args4j.CmdLineException;
+
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,22 +11,20 @@ import java.util.Date;
 import java.util.List;
 
 
+
 public class CommandLs {
-
-    public static String pathDirectoryOutput = "./src/main/resources/output/output.txt";
-
-   public static String pathDirectory;
-    private List<String> result;
-    private ArrayList<File> infoOrdered;
+    private static String pathDirectory;
+    private static ArrayList result;
+    private static ArrayList<File> infoOrdered;
 
 
     public CommandLs(String currentDirectory) {
-        this.pathDirectory = currentDirectory;
-        this.result = new ArrayList();
-        this.infoOrdered = new ArrayList<>();
+        pathDirectory = currentDirectory;
+        result = new ArrayList<>();
+        infoOrdered = new ArrayList<>();
     }
 
-    public void validateDirectory() {
+    private void validateDirectory() {
         infoOrdered = new ArrayList<>();
         File file = new File(pathDirectory);
         File[] listDocs = file.listFiles();
@@ -31,119 +33,91 @@ public class CommandLs {
             Collections.addAll(infoOrdered, listDocs);
         } else if (file.isFile()) {
             infoOrdered.add(file);
-            Collections.sort(infoOrdered);
+            Collections.addAll(infoOrdered);
         }
     }
 
-    public void getCommand_l() {
+    private static void getCommand_l() {
         for (File document : infoOrdered) {
             String name = document.getName();
             String permission = getPermission(document);
             String sizeFile = String.valueOf(document.length()).concat(" ").concat("Bytes");
             String dateFile = getDate(document);
             String line = name.concat(" ").concat(permission).concat(" ").concat(dateFile).concat(" ").concat(sizeFile);
-            this.result.add(line);
+            result.add(line);
         }
-        printConsole();
     }
 
-    public void getCommand_h() {
+    private static void getCommand_h() {
         for (File document : infoOrdered) {
             String name = document.getName();
-            this.result.add(name);
+            result.add(name);
         }
-        printConsole();
     }
 
-    public void getCommand_r() {
-        for (File document : infoOrdered  ) {
-            String name = document.getName();
-            this.result.add(name);
-        }
-                Collections.reverse(result);
-        printConsole();
+    private static void getCommand_r() {
+        ArrayList<String> r = new ArrayList<>();
+        if (result.isEmpty()) {
+            for (File document : infoOrdered) {
+                String name = document.getName();
+                r.add(name);
             }
+            Collections.reverse(r);
+        } else {
+            Collections.reverse(result);
+
+        }
+    }
 
 
+    private static void getCommand_o(ArrayList<String> list, File pathDirectoryOutput, boolean o) {
+        if (o) {
+            BufferedWriter writer;
+            try {
+                writer = Files.newBufferedWriter(Paths.get(pathDirectoryOutput.toURI()));
 
-    public void getCommand_o() {
-        PrintWriter writer;
-        try {
-            writer = new PrintWriter(pathDirectoryOutput, "UTF-8");
-
-            for (String line : this.result) {
-                writer.println(line);
+                for (String line : list) {
+                    writer.write(line);
+                    writer.newLine();
+                }
+                writer.flush();
+                writer.close();
+            } catch (IOException ex) {
+                System.out.println(":(");
             }
-        } catch (IOException ex) {
-            System.out.println(":(");
+        } else {
+            for (String info : list) System.out.println(info);
         }
     }
 
-
-    public void printConsole() {
-        for (String line : this.result) {
-            System.out.println(line);
-        }
-    }
-
-
-    public String getResult() {
-        StringBuilder s = new StringBuilder();
-        for (String line : this.result) {
-            s = s.append(line).append("\n");
-        }
-        return s.toString();
-    }
-
-    public String getPermission(File document) {
+    private static String getPermission(File document) {
         String permission = (document.canRead()) ? "1" : "0";
         permission = permission.concat((document.canWrite()) ? "1" : "0");
         permission = permission.concat((document.canExecute()) ? "1" : "0");
         return permission;
     }
 
-    public String getRWX(File document) {
+    private static String getRWX(File document) {
         String permission = (document.canRead()) ? "r" : "_";
         permission = permission.concat((document.canWrite()) ? "w" : "_");
         permission = permission.concat((document.canExecute()) ? "x" : "_");
         return permission;
     }
 
-    public String getSizeHumanReadable(long sizeDoc) {
+    private static String getSizeHumanReadable(long sizeDoc) {
         long KB = 1024L;
         long MB = 1024 * KB;
         long GB = 1024 * MB;
         if (sizeDoc > GB) {
-            return new Float((double)sizeDoc / GB).toString().concat(" GB");
+            return new Float((double) sizeDoc / GB).toString().concat(" GB");
         }
         if (sizeDoc > MB) {
             return new Float((double) sizeDoc / MB).toString().concat(" MB");
         }
-        return new Float((double)sizeDoc / KB).toString().concat(" KB");
+        return new Float((double) sizeDoc / KB).toString().concat(" KB");
     }
 
-    public void getCommandlAndh(boolean l, boolean h, boolean r, boolean o) throws IOException{
-        for (File document : infoOrdered) {
-            String name = document.getName();
-            if (l && h) {
-                String permission = getRWX(document);
-                String dateFile = getDate(document);
-                String sizeFile = getSizeHumanReadable(document.length());
-                String line = name.concat(" ").concat(permission).concat(" ").concat(dateFile).concat(" ").concat(sizeFile);
-                this.result.add(line);
-            }
-        }
-            if (r) {
-                Collections.reverse(result);
-                printConsole();
-            }
-            if (o) {
-                getCommand_o();
-            }
-    }
-
-
-    public String getDate(File file) {
+    private static String getDate(File file) {
         long time = file.lastModified();
         Date date = new Date();
         date.setTime(time);
@@ -151,46 +125,55 @@ public class CommandLs {
         return format.format(date);
     }
 
-    public void process(String[] args) throws IOException{
-        if (args.length > 0) {
-            if (new File(pathDirectory).exists()) {
-                validateDirectory();
-                if (args[0].equals("ls")) {
-                    if (args.length == 2) {
-                        if (args[1].equals("-l")) getCommand_l();
-                        else if (args[1].equals("-h")) getCommand_h();
-                        else if (args[1].equals("-r")) getCommand_r();
-                    } else if (args[1].equals("-l") && (args.length == 3 && args[2].equals("-h"))) {
-                        getCommandlAndh(true, true, false, false);
-                    } else if (args[1].equals("-l") && args.length == 4 && args[2].equals("-h") && args[3].equals("-r")) {
-                        getCommandlAndh(true, true, true, false);
-
-                    } else if (args[1].equals("-l") && args.length == 5 && args[2].equals("-h") && args[3].equals("-r") && args[4].equals("-o")) {
-                        getCommandlAndh(true, true, true, true);
-
-                    }
-                    //write result in file
-                    else if (args.length == 3 && args[2].equals("-o")) {
-                        getCommand_o();
-                    }
-                } else {
-                    System.out.print("Argument no valid");
+    private static List<String> info(boolean l, boolean h, boolean r) {
+            if (l && h) {
+                for (File document : infoOrdered) {
+                    String name = document.getName();
+                    String permission = getRWX(document);
+                    String dateFile = getDate(document);
+                    String sizeFile = getSizeHumanReadable(document.length());
+                    String line = name.concat(" ").concat(permission).concat(" ").concat(dateFile).concat(" ").concat(sizeFile);
+                    result.add(line);
                 }
-            } else {
-                System.out.println("selected file path does not exist: ");
-                System.out.println(new File(pathDirectory));
+            } else if (l) {
+                getCommand_l();
+            } else if (h) {
+                getCommand_h();
             }
-        }
+
+            if (r) {
+                getCommand_r();
+            }
+        return result;
     }
 
-    public static void main(String[] args) {
-        CommandLs commandLs = new CommandLs(pathDirectory);
+    public void process (boolean l, boolean h, boolean r, boolean o, File pathDirectoryOutput) throws IOException {
+        if (new File(pathDirectory).exists()) {
+            validateDirectory();
+            List<String> list = info(l,h,r);
+            getCommand_o((ArrayList<String>) list, pathDirectoryOutput,o);
+        } else {
+            System.out.println("selected file path does not exist: ");
+            System.out.println(new File(pathDirectory));
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException, CmdLineException {
+        CommandLineArgument values = new CommandLineArgument(args);
+        boolean l = values.longFormat;
+        boolean h = values.humanReadable;
+        boolean r = values.reverse;
+        boolean o = values.out != null;
+        String pd = values.pathFile;
+        File pathDirectoryOutput = new File ("./src/main/resources/output/" + values.out);
+
+        CommandLs commandLs = new CommandLs(pd);
         try {
-            commandLs.process(args);
+            commandLs.process(l,h,r,o,pathDirectoryOutput);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
 }
-
